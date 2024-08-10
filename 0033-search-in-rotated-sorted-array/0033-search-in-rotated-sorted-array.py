4dd7794e54a49ad4bd5a7f463d58dca18e903a28
Ayman Issa <aymanadel003@gmail.com>
2024-08-10 11:40:56 +0200

    Time: 25 ms (39.53%), Space: 11.7 MB (98.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,26 +5,6 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
-        left, right = 0, len(nums) - 1
-
-        while left <= right:
-            mid = (left + right) // 2
-            curr = nums[mid]
-
-            if curr == target:
-                return mid
-
-            # Determine which side is sorted
-            if nums[left] <= curr:  # Left side is sorted
-                if nums[left] <= target < curr:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            else:  # Right side is sorted
-                if curr < target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-
+        if target in nums:
+            return nums.index(target)
         return -1
