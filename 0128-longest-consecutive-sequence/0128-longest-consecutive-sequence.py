cff4d543882df77e9383c163cd23148ef6bf7fe9
Ayman Issa <aymanadel003@gmail.com>
2024-09-25 15:03:06 +0200

    Time: 358 ms (64.28%), Space: 28.8 MB (71.86%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,20 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums_set = set(nums)
+        nums = set(nums)
         output = 0
 
-        for num in nums_set:
-            if num - 1 not in nums_set:
-                curr = num
-                counter = 1
-
-                while curr + 1 in nums_set:
-                    curr += 1
-                    counter += 1
+        for num in nums:
+            if num - 1 not in nums:
+                curr_counter = 1
+                curr_num = num + 1  
                 
-                output = max(output, counter)
+                while curr_num in nums:
+                    curr_counter += 1
+                    curr_num += 1
 
+                output = max(output, curr_counter)
 
         return output
-        
