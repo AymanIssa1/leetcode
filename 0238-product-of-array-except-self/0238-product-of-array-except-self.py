894f3c46783f4a3784d6c52f1d7e94bb5e5d5cb6
Ayman Issa <aymanadel003@gmail.com>
2024-09-25 13:10:56 +0200

    Time: 278 ms (80.99%), Space: 19.2 MB (58.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,28 +5,27 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        zeros = 0
+        output = [0] * n
+        all_product = 1
+        zeros_count = 0
 
         for num in nums:
             if num == 0:
-                zeros += 1
-        if zeros > 1:
-            return [0] * n
-        
-        output = [0] * n 
-        total_product = 1
-
-        for num in nums:
-            if num != 0:
-                total_product *= num
-        print(total_product)
-        for i in range(n):
-            print(i)
-            if zeros == 1 and nums[i] != 0:
-                output[i] = 0
-            elif zeros == 1 and nums[i] == 0:
-                output[i] = total_product
+                zeros_count += 1
             else:
-                output[i] = total_product / nums[i]
+                all_product *= num
         
+        if zeros_count > 1:
+            return output
+
+        if zeros_count == 1:
+            for i in range(n):
+                if nums[i] != 0:
+                    continue
+                output[i] = all_product
+        else:
+            for i in range(n):
+                output[i] = all_product // nums[i]
+
         return output
+        
