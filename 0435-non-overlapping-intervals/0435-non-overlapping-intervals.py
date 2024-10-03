a316084b24d6a08109a098e8dfa99f78f13a4e92
Ayman Issa <aymanadel003@gmail.com>
2024-10-03 12:24:37 +0200

    Time: 1155 ms (41.63%), Space: 57.9 MB (73.72%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -4,17 +4,20 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        intervals.sort(key=lambda x: x[1])
+        intervals.sort(key=lambda x:x[1]) 
         output = 0
 
-        last_end = intervals[0][1]
+        prev_end = intervals[0][1]
 
         for i in range(1, len(intervals)):
             curr_start, curr_end = intervals[i]
 
-            if curr_start < last_end:
+            if curr_start < prev_end:
                 output += 1
             else:
-                last_end = curr_end
-        
+                prev_end = curr_end
+
         return output
+
+# [[1,100],[11,22],[1,11],[2,12]]
+# [[1, 11], [2, 12], [11, 22], [1, 100]]
