ff83cb8079f38d266f1bede104469b65533868a7
Ayman Issa <aymanadel003@gmail.com>
2024-12-02 14:52:48 +0100

    Time: 23 ms (51.81%), Space: 13.1 MB (9.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -12,7 +12,6 @@ class Solution(object):
                 start = max(start, memo[s[end]] + 1)
             max_len = max(max_len, (end - start + 1))
             memo[s[end]] = end
-            end += 1
 
         return max_len
         
