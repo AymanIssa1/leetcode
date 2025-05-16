043b7a83014e22017a6da8c15aace3abce4328ef
Ayman Issa <aymanadel003@gmail.com>
2025-05-16 21:31:54 +0300

    Time: 3553 ms (14.31%), Space: 15 MB (100%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,30 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()
         n = len(nums)
         output = []
+        
+        nums.sort()
 
-        for i in range(n-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-
-            left, right = i+1, n-1
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # skip duplicate elements
+                
+            left = i + 1
+            right = n - 1
 
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
                 if total == 0:
-                    sub_arr = [nums[i], nums[left], nums[right]]
-                    if sub_arr not in output:
-                        output.append(sub_arr)
+                    arr = [nums[i], nums[left], nums[right]]
+                    if arr not in output:
+                        output.append(arr)
+                
+                if total > 0:
                     right -= 1
+                elif total < 0:
                     left += 1
-                elif total > 0:
-                    right -= 1
                 else:
+                    arr = [nums[i], nums[left], nums[right]]
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1  # skip duplicate elements
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1  # skip duplicate elements
                     left += 1
+                    right -= 1
+                
 
         return output
-            
-
         
